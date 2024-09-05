a2111775c1efebd06bed8e91247d8e4d38573bd6
Hart Nurnberg <hartnurnberg@gmail.com>
2024-09-05 10:49:01 -0500

    Replace code with string manipulator code.

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,19 @@
-print("Bye World!")
+def main():
+    word = input('Enter a string: ')
+    new_word = ''
+
+    for char in word:
+        if 'a' <= char <= 'z':
+            new_word += chr(ord(char)-32)
+        else:
+            new_word += char
+
+    print('The capitalized word is:', new_word)
+
+    rev_word = ''
+    for char in new_word[::-1]:
+        rev_word += char
+
+    print('The reversed word is:', rev_word)
+
+main()
